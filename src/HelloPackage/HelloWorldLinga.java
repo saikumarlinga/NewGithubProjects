dcf98693d016c51bcdbfe70d7e6433d47ddc2008
Kalyan <Kalyan@DESKTOP-HIGVP14>
2022-06-21 14:12:20 +0530

    kalyan j

--- src/HelloPackage/HelloWorldLinga.java
+++ src/HelloPackage/HelloWorldLinga.java
@@ -5,6 +5,7 @@ public class HelloWorldLinga {
 	public static void main(String[] args) {
 		System.out.println("helloworld");
 		System.out.println("nuv pampina class change chesi pamputuna chudu ra");
+		System.out.println("edit chesina chudu ra");
 	}
 
 }
