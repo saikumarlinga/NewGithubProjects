07e7b322756233d9f995fafac15c730c33aec2f4
ADMIN <ADMIN@DESKTOP-DKDQVD8>
2022-06-21 12:54:37 +0530

    shared

--- src/HelloPackage/HelloWorldLinga.java
+++ src/HelloPackage/HelloWorldLinga.java
@@ -4,6 +4,7 @@ public class HelloWorldLinga {
 
 	public static void main(String[] args) {
 		System.out.println("helloworld");
+		System.out.println("nuv pampina class change chesi pamputuna chudu ra");
 	}
 
 }
